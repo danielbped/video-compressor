3d0a604f6d0bf31d504fb4def702565ef8c42929
Daniel Batista <danielbped@gmail.com>
2025-08-18 19:25:45 -0300

    feat: adds middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,56 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { config } from 'dotenv';
+import { UserModule } from './routes/user/user.module';
+import User from './entity/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './routes/user/user.controller';
+import { UserService } from './routes/user/user.service';
+import { UserMiddleware } from './middleware/user.middleware';
+import { AuthenticationMiddleware } from './middleware/authentication.middleware';
+
+config();
+
+const {
+  MYSQL_DB_USER,
+  MYSQL_DB_PASSWORD,
+  MYSQL_DB_NAME,
+  MYSQL_DB_HOST,
+  MYSQL_DB_PORT,
+} = process.env;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: MYSQL_DB_HOST,
+      port: Number(MYSQL_DB_PORT),
+      username: MYSQL_DB_USER,
+      password: MYSQL_DB_PASSWORD,
+      database: MYSQL_DB_NAME,
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UserMiddleware)
+      .forRoutes({ path: 'user', method: RequestMethod.POST });
+
+    consumer.apply(AuthenticationMiddleware).forRoutes('conversation', {
+      path: 'message',
+      method: RequestMethod.POST,
+    });
+  }
+}
---
+++ src/interface/response.interface.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import User from '../entity/user.entity';
+
+export interface HealthResponse {
+  message: string;
+}
+
+export interface LoginResponse {
+  token: string;
+  user: Partial<User>;
+}
+
+export interface CreateUserResponse {
+  user: Partial<User>;
+  token: string;
+}
