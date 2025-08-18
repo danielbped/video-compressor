b4f74f2650f11705e32dd585f85db702b2d26a86
Daniel Batista <danielbped@gmail.com>
2025-08-18 18:19:15 -0300

    feat: Update package dependencies and implement video compression feature

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,71 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { File as MulterFile } from 'multer';
+import { join, parse } from 'path';
+import * as fs from 'fs';
+import ffmpeg from 'fluent-ffmpeg';
 
-@Controller()
+@Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Post('videos')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (_req, file, cb) => {
+          const filename = `${Date.now()}-${file.originalname}`;
+          cb(null, filename);
+        },
+      }),
+    }),
+  )
+  async uploadFile(@UploadedFile() file: MulterFile) {
+    const inputPath = file.path;
+    const { name, ext } = parse(file.filename);
+    const compressedFilename = `${name}_low${ext}`;
+    const outputPath = join('./uploads', compressedFilename);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    try {
+      await new Promise<void>((resolve, reject) => {
+        ffmpeg(inputPath)
+          .outputOptions([
+            '-vcodec libx264',
+            '-crf 28',
+            '-preset veryfast',
+            '-acodec aac',
+            '-b:a 128k',
+          ])
+          .on('end', () => {
+            console.log('Compressão concluída:', outputPath);
+            resolve();
+          })
+          .on('error', (err) => {
+            console.error('Erro durante a compressão:', err);
+            reject(err);
+          })
+          .save(outputPath);
+      });
+
+      fs.unlinkSync(inputPath);
+
+      return {
+        message: 'Vídeo comprimido com sucesso!',
+        filename: compressedFilename,
+        path: outputPath,
+      };
+    } catch (error) {
+      console.error('Erro no processamento do vídeo:', error);
+      throw error;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Request } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  compressVideo(_req: Request): string {
+    return 'Compressing...!';
   }
 }
